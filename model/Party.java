f39bd07e1d533e62951ba91aecf1659e7afe3381
Saby-AI <sabyasachi.s@encora.com>
2025-09-04 18:47:14 +0530

    Add files via upload

---
+++ model/Party.java
@@ -0,0 +1,42 @@
+package model
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class party {
+ static int id=1
+
+ int partyid;
+ String partynumber
+ String partyname
+ String partytype
+ String partygroup
+ String partystatus
+
+ List<Address> addresses = new ArrayList()
+ List<Communication> communications = new ArrayList()
+
+ party(String name,String type,String group,String status){
+  partyid=id++
+  partynumber=genNumber(group)
+  partyname=name
+  partytype=type
+  partygroup=group
+  partystatus=status
+ }
+
+ String genNumber(String g){
+  return g.substring(0,2).toUpperCase()+"_"+System.currentTimeMillis()
+ }
+
+ void addAddress(Address a){
+  addresses.add(a)
+ }
+
+ void addCommunication(Communication c){
+  communications.add(c)
+ }
+
+ public String tostring(){
+  return "party {id:"+partyid+", number:"+partynumber+", name:"+partyname+", type:"+partytype+", group:"+partygroup+", status:"+partystatus+", addresses:"+addresses+", communications:"+communications+"}"
+ }
