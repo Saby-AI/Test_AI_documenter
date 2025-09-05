26e0675146ac7ee538fae72be09a5f8f753e2da7
Saby-AI <sabyasachi.s@encora.com>
2025-09-05 20:46:24 +0530

    Updated Party.java with refactored code

---
+++ model/Party_refactored.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Party {
+    private static int id = 1;
+
+    int partyid;
+    String partynumber;
+    String partyname;
+    String partytype;
+    String partygroup;
+    String partystatus;
+
+    List<Address> addresses = new ArrayList<>();
+    List<Communication> communications = new ArrayList<>();
+
+    Party(String name, String type, String group, String status) {
+        partyid = id++;
+        partynumber = genNumber(group);
+        partyname = name;
+        partytype = type;
+        partygroup = group;
+        partystatus = status;
+    }
+
+    String genNumber(String g) {
+        return g.substring(0, 2).toUpperCase() + "_" + System.currentTimeMillis();
+    }
+
+    void addAddress(Address a) {
+        addresses.add(a);
+    }
+
+    void addCommunication(Communication c) {
+        communications.add(c);
+    }
+
+    public String toString() {
+        return "party {id:" + partyid + ", number:" + partynumber + ", name:" + partyname + ", type:" + partytype + ", group:" + partygroup + ", status:" + partystatus + ", addresses:" + addresses + ", communications:" + communications + "}";
+    }
+}
