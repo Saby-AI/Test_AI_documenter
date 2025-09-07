8f475813c5cae84651d5524eecdb5fe91748b884
Saby-AI <sabyasachi.s@encora.com>
2025-09-07 22:07:52 +0530

    Refactored Party.java based on review suggestions.

--- model/Party.reviewed.java
+++ model/Party.reviewed.java
@@ -1,41 +1,31 @@
 package model;
-import java.util.ArrayList;
 import java.util.List;
 public class Party {
-    private static int nextId = 1;
-    private int partyId;
-    private String partyNumber;
-    private String partyName;
-    private String partyType;
-    private String partyGroup;
-    private String partyStatus;
-    private List<Address> addresses;
-    private List<Communication> communications;
-    public Party(String name, String type, String group, String status) {
-        this.partyId = nextId++;
-        this.partyNumber = genNumber(group);
-        this.partyName = name;
-        this.partyType = type;
-        this.partyGroup = group;
-        this.partyStatus = status;
-        this.addresses = new ArrayList<>();
-        this.communications = new ArrayList<>();
+    private final List<String> guests;
+    private String location;
+    private String time;
+    public Party(List<String> guests, String location, String time) {
+        this.guests = guests;
+        this.location = location;
+        this.time = time;
     }
-    private String genNumber(String group) {
-        return group.substring(0, 2).toUpperCase() + "_" + System.currentTimeMillis();
-    }
-    public void addAddress(Address address) {
-        if (address != null) {
-            addresses.add(address);
+    public boolean addGuest(String guest) {
+        if (!guests.contains(guest)) {
+            guests.add(guest);
+            return true;
         }
+        return false;
     }
-    public void addCommunication(Communication communication) {
-        if (communication != null) {
-            communications.add(communication);
-        }
+    public boolean removeGuest(String guest) {
+        return guests.remove(guest);
     }
-    @Override
-    public String toString() {
-        return "Party {id:" + partyId + ", number:" + partyNumber + ", name:" + partyName + ", type:" + partyType + ", group:" + partyGroup + ", status:" + partyStatus + ", addresses:" + addresses + ", communications:" + communications + "}";
+    public List<String> getGuests() {
+        return guests;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public String getTime() {
+        return time;
     }
 }
