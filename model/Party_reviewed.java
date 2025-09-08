4850a79b2b52f9180910783b4f843f1a6387213c
Saby-AI <sabyasachi.s@encora.com>
2025-09-08 11:46:53 +0530

    Refactored Party.java for improved method responsibilities

--- model/Party_reviewed.java
+++ model/Party_reviewed.java
@@ -1,1 +1,20 @@
-Could not detect the programming language from the prompt. Please include a filename with a known extension or mention the language.
+public class Party {
+    private String name;
+    private int guests;
+    public Party(String name, int guests) {
+        this.name = name;
+        this.guests = guests;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getGuests() {
+        return guests;
+    }
+    public String formatDetails() {
+        return "Party Name: " + name + ", Number of Guests: " + guests;
+    }
+    public void printDetails() {
+        System.out.println(formatDetails());
+    }
+}
