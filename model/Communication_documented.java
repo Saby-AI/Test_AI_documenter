2d59065512f7bceea647b7e9d9f96de5f921c8ed
Saby-AI <sabyasachi.s@encora.com>
2025-09-13 21:25:28 +0530

    Add documented version of model/Communication.java

---
+++ model/Communication_documented.java
@@ -0,0 +1,9 @@
+/**
+     * Returns a string representation of the Communication object for logging/display.
+     *
+     * @return String representation of the Communication object
+     */
+    public String toString() {
+        return "Comm{ type: " + contactType + ", contact: " + contact + " }";
+    }
+}
