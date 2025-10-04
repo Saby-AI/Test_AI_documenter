eadc88d06ad42fb2a2e22b7013137d661ab2dc70
Saby-AI <sabyasachi.s@encora.com>
2025-10-04 18:01:50 +0530

    Add documented version of model/Party.java

--- model/Party_documented.java
+++ model/Party_documented.java
@@ -1,13 +1,23 @@
-/**
-     * Formats details of the party into a string representation.
+// public void setGuests(int guests) {
+    // }
+    /**
+     * Gets the number of guests at the party.
      *
-     * @return formatted string containing party details
+     * @return The number of guests
+     */
+    public int getGuests() {
+        return guests;
+    }
+    /**
+     * Formats the details of the party into a string.
+     *
+     * @return A string representing the party details
      */
     public String formatDetails() {
         return "Party Name: " + name + ", Number of Guests: " + guests;
     }
     /**
-     * Prints the details of the party to the console in a readable format.
+     * Prints the details of the party to the console.
      */
     public void printDetails() {
         StringBuilder sb = new StringBuilder();
