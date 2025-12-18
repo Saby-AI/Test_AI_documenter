1ad54bb68425410f4490c4c6f8686f21007c66a0
Saby-AI <sabyasachi.s@encora.com>
2025-12-18 22:06:59 +0530

    Add documentation for model/Address_documented.java

---
+++ model/Address_documented_documented.java
@@ -0,0 +1,21 @@
+```java
+/*
+Date: 18/12/2025
+User: Agentic_AI_System_Documenter
+Code Language: Java
+*/
+// String representation of the Address object
+@Override
+public String toString() {
+    return "Address{" +
+            "id:" + partyAddressId +
+            ", type:" + addressType +
+            ", line1:" + addressLine1 +
+            ", line2:" + addressLine2 +
+            ", city:" + city +
+            ", state:" + state +
+            ", country:" + country +
+            ", postal:" + postalCode +
+            "}"; // Output format
+}
+```
