00cfdadeb4ae7f20c00a3f7af3fef4008a14a58f
Saby-AI <sabyasachi.s@encora.com>
2025-12-18 22:07:01 +0530

    Add documentation for model/Party.java

--- model/Party_documented.java
+++ model/Party_documented.java
@@ -1,15 +1,62 @@
-// Getter for number of guests
+```java
+/*
+Date: 18/12/2025
+User: Agentic_AI_System_Documenter
+Code Language: Java
+*/
+/**
+ * The Party class represents a party with a name and a number of guests.
+ */
+public class Party {
+    private String name; // The name of the party
+    private int guests;  // The number of guests attending the party
+    /**
+     * Constructor to initialize the Party object with a name and number of guests.
+     *
+     * @param name   The name of the party.
+     * @param guests The number of guests attending the party.
+     */
+    public Party(String name, int guests) {
+        this.name = name;
+        this.guests = guests;
+    }
+    // Setter for name (currently commented out)
+    // public void setName(String name) {
+    // }
+    /**
+     * Gets the name of the party.
+     *
+     * @return The name of the party.
+     */
+    public String getName() {
+        return name;
+    }
+    // Setter for guests (currently commented out)
+    // public void setGuests(int guests) {
+    // }
+    /**
+     * Gets the number of guests attending the party.
+     *
+     * @return The number of guests.
+     */
     public int getGuests() {
-        return guests; // Return the number of guests
+        return guests;
     }
-    // Format party details for display
+    /**
+     * Formats the details of the party into a string.
+     *
+     * @return A string representation of the party details.
+     */
     public String formatDetails() {
-        return "Party Name: " + name + ", Number of Guests: " + guests; // Return formatted details
+        return "Party Name: " + name + ", Number of Guests: " + guests;
     }
-    // Print party details to console
+    /**
+     * Prints the details of the party to the console.
+     */
     public void printDetails() {
-        StringBuilder sb = new StringBuilder(); // StringBuilder for efficient strings
-        sb.append("Party Name: ").append(name).append(", Number of Guests: ").append(guests); // Build string
-        System.out.println(sb.toString()); // Output party details
+        StringBuilder sb = new StringBuilder();
+        sb.append("Party Name: ").append(name).append(", Number of Guests: ").append(guests);
+        System.out.println(sb.toString());
     }
 }
+```
